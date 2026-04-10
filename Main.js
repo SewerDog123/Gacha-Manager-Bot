c8d06ac1d60f467e9d67f9d6b108458d70f9a941
SewerDog123 <nnthxa729@gmail.com>
2026-04-10 18:18:50 +0700

    changed command path to 'src', 'commands'

--- Main.js
+++ Main.js
@@ -14,7 +14,7 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, 'src', 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
